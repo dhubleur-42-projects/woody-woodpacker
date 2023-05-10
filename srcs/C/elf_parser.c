daba31381a6303037bd7ecc032c77ff56e4ad873
dhubleur <hubleur.damien333@protonmail.ch>
2023-05-10 14:46:26 +0200

    feat(PARSER): Full parsing of ELF headers

--- srcs/C/elf_parser.c
+++ srcs/C/elf_parser.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 13:56:27 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/05/10 14:36:46 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/05/10 14:45:35 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 bool parse_header(char *name, Elf64_Ehdr *header_ptr, int *fd);
 bool parse_sections_header(int fd, Elf64_Ehdr header, Elf64_Shdr **sections);
+bool parse_programs_header(int fd, Elf64_Ehdr header, Elf64_Phdr **program);
 
 void parse_file(char *name) {
 	int fd;
@@ -21,15 +22,27 @@ void parse_file(char *name) {
 	if (!parse_header(name, &header, &fd))
 		return ;
 
-	Elf64_Shdr *sections_header;
-	if (!parse_sections_header(fd, header, &sections_header))
+	Elf64_Phdr *program_header;
+	if (!parse_programs_header(fd, header, &program_header))
 	{
 		close(fd);
 		return ;
 	}
+
+	Elf64_Shdr *sections_header;
+	if (!parse_sections_header(fd, header, &sections_header))
+	{
+		free(program_header);
+		close(fd);
+		return ;
+	}
+
+	close(fd);
+	free(program_header);
+	free(sections_header);
 }
 
-bool parse_header(char *name, Elf64_Ehdr *header_ptr, int *fd) {
+bool parse_header(char *name, Elf64_Ehdr *header, int *fd) {
 	*fd = open(name, O_RDONLY);
 	if (*fd == -1)
 	{
@@ -37,8 +50,7 @@ bool parse_header(char *name, Elf64_Ehdr *header_ptr, int *fd) {
 		return false;
 	}
 
-	Elf64_Ehdr header = *header_ptr;
-	int ret = read(*fd, &header, sizeof(Elf64_Ehdr));
+	int ret = read(*fd, header, sizeof(Elf64_Ehdr));
 	if (ret == -1)
 	{
 		perror("Cannot read file");
@@ -52,14 +64,14 @@ bool parse_header(char *name, Elf64_Ehdr *header_ptr, int *fd) {
 		close (*fd);
 		return false;
 	}
-	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 || header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
+	if (header->e_ident[EI_MAG0] != ELFMAG0 || header->e_ident[EI_MAG1] != ELFMAG1 || header->e_ident[EI_MAG2] != ELFMAG2 || header->e_ident[EI_MAG3] != ELFMAG3)
 	{
 		dprintf(2, "Invalid file (Invalid magic numbers)\n");
 		close (*fd);
 		return false;
 	}
 
-	if (header.e_ident[EI_CLASS] != ELFCLASS64)
+	if (header->e_ident[EI_CLASS] != ELFCLASS64)
 	{
 		dprintf(2, "Not a 64 bits file\n");
 		close (*fd);
@@ -70,41 +82,81 @@ bool parse_header(char *name, Elf64_Ehdr *header_ptr, int *fd) {
 	return true;
 }
 
-bool parse_sections_header(int fd, Elf64_Ehdr header, Elf64_Shdr **sections_ptr) {
-	if (header.e_shnum == 0 || header.e_shentsize == 0 || header.e_shoff == 0)
+bool parse_programs_header(int fd, Elf64_Ehdr header, Elf64_Phdr **program) {
+	if (header.e_phnum == 0 || header.e_phentsize == 0 || header.e_phoff == 0)
 	{
-		*sections_ptr = NULL;
-		printf("No sections header\n");
+		*program = NULL;
+		printf("No program header\n");
 		return true;
 	}
 
-	*sections_ptr = malloc(header.e_shentsize * header.e_shnum);
-	if (*sections_ptr == NULL)
+	*program = malloc(header.e_phentsize * header.e_phnum);
+	if (*program == NULL)
 	{
 		perror("Malloc failed");
 		return false;
 	}
 
-	Elf64_Shdr *sections = *sections_ptr;
+	int ret = lseek(fd, header.e_phoff, SEEK_SET);
+	if (ret == -1)
+	{
+		perror("Cannot seek to program header");
+		free(*program);
+		return false;
+	}
+
+	ret = read(fd, *program, header.e_phentsize * header.e_phnum);
+	if (ret == -1)
+	{
+		perror("Cannot read program header");
+		free(*program);
+		return false;
+	}
+	if (ret != header.e_phentsize * header.e_phnum)
+	{
+		dprintf(2, "Invalid file (Impossible to read complete program header)\n");
+		free(*program);
+		return false;
+	}
+
+	printf("Valid programs header\n");
+	return true;
+}
+
+bool parse_sections_header(int fd, Elf64_Ehdr header, Elf64_Shdr **sections) {
+	if (header.e_shnum == 0 || header.e_shentsize == 0 || header.e_shoff == 0)
+	{
+		*sections = NULL;
+		printf("No sections header\n");
+		return true;
+	}
+
+	*sections = malloc(header.e_shentsize * header.e_shnum);
+	if (*sections == NULL)
+	{
+		perror("Malloc failed");
+		return false;
+	}
+
 	int ret = lseek(fd, header.e_shoff, SEEK_SET);
 	if (ret == -1)
 	{
 		perror("Cannot seek to sections header");
-		free(sections);
+		free(*sections);
 		return false;
 	}
 
-	ret = read(fd, sections, header.e_shentsize * header.e_shnum);
+	ret = read(fd, *sections, header.e_shentsize * header.e_shnum);
 	if (ret == -1)
 	{
 		perror("Cannot read sections header");
-		free(sections);
+		free(*sections);
 		return false;
 	}
 	if (ret != header.e_shentsize * header.e_shnum)
 	{
 		dprintf(2, "Invalid file (Impossible to read complete sections header)\n");
-		free(sections);
+		free(*sections);
 		return false;
 	}
 
