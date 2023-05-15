623181b09be1d66161872715c195b4af41e71e13
Jérémy M <jeremy.maia@protonmail.com>
2023-05-15 15:24:18 +0200

    Create do_first_round() and do_last_round()

--- srcs/C/encryption/encryption_utils.h
+++ srcs/C/encryption/encryption_utils.h
@@ -7,6 +7,9 @@
 void	shift_rows(uint8_t *box);
 void	mix_columns(uint8_t *box);
 void	generate_keys(uint8_t *initial_key, uint8_t *subkeys);
+void	do_first_round(uint8_t *block, uint8_t *initial_key);
+void	do_last_round(uint8_t *block, uint8_t *key);
+void	do_regular_round(uint8_t *block, uint8_t *key);
 
 void	sub_word(uint32_t *word);
 void	word_left_shift(uint32_t *word);
@@ -18,5 +21,6 @@ void	right_shift(uint8_t *array, size_t len);
 int		am_i_little_endian(void);
 void	up_shift(uint8_t *array, size_t width, size_t height, size_t i_column);
 void	n_up_shift(uint8_t *array, size_t width, size_t height, size_t i_column, size_t n_times);
+void	xor_bytes(uint8_t *a, uint8_t *b, size_t n_bytes);
 
 #endif
--- srcs/C/encryption/xor_bytes.c
+++ srcs/C/encryption/xor_bytes.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   make_a_round.c                                     :+:      :+:    :+:   */
+/*   xor_bytes.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/12 15:10:05 by jmaia             #+#    #+#             */
-/*   Updated: 2023/05/12 19:19:36 by jmaia            ###   ###               */
+/*   Created: 2023/05/15 14:43:07 by jmaia             #+#    #+#             */
+/*   Updated: 2023/05/15 14:44:12 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "encryption_utils.h"
 
-// We assume block has a length of 16 bytes and key has a length of 16 bytes
-void	make_a_round(uint8_t *block, uint8_t *key)
+void	xor_bytes(uint8_t *a, uint8_t *b, size_t n_bytes)
 {
-	sub_bytes(block, 16);
-	shift_rows(block);
-	mix_columns(block);
-	for (int i = 0; i < 16; i++)
-		block[i] ^= key[i];
+	for (size_t i = 0; i < n_bytes; i++)
+		a[i] ^= b[i];
 }
