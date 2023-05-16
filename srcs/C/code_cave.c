344517d06bbb168dcdca288c71880197faa73575
Damien <hubleur.damien333@protonmail.ch>
2023-05-16 20:46:13 +0200

    no more fucking segfault but bad instrcution (I suspect the payload)

--- srcs/C/code_cave.c
+++ srcs/C/code_cave.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:54:29 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/05/16 19:34:45 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/05/16 20:42:53 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 const char payload[] = "\x31\xc0\x31\xdb\x31\xd2\x68\x2e\x2e\x57\x4f\x4f\x44\x59\x2e\x2e\xc6\x44\x24\x03\x0a\x68\x57\x4f\x4f\x44\x68\x2e\x2e\x2e\x2e\x89\xe1\xb2\x0c\xb0\x04\xb3\x01\xcd\x80\xb2\x0c\x01\xd4";
 
-char jmp[] = "xe9xffxffxffxff";                      
-char pusha[] = "x60";
-char popa[] = "x61";
+char jmp[] = "\xe9\xff\xff\xff\xff";                      
+char pusha[] = "\x60";
+char popa[] = "\x61";
 
 #define CODE_SIZE (sizeof(payload)-1 + sizeof(jmp)-1 + sizeof(pusha)-1 + sizeof(popa)-1)
 
-#define CODE_OFFSET (program_headers->p_offset + program_headers->p_memsz)
+#define CODE_OFFSET (selected_header->p_offset + selected_header->p_memsz)
 
 void insert_code(unsigned char *ptr)
 {
@@ -37,39 +37,32 @@ void insert_code(unsigned char *ptr)
 }
 
 void create_codecave(Elf64_Ehdr *header, Elf64_Shdr *section_headers, Elf64_Phdr *program_headers, void *output_file_map, off_t output_file_size) {
-    (void)header;
 	(void)section_headers;
-	(void)program_headers;
-	(void)output_file_map;
 	(void)output_file_size;
 
 	unsigned int last_entry = header->e_entry;
 
-	for(int i = 0; i < header->e_phnum - 1; i++)
+	int i = 0;
+	for(; i < header->e_phnum - 1; i++)
     {
-        if(program_headers->p_type == PT_LOAD)
+        if(program_headers[i].p_type == PT_LOAD && program_headers[i+1].p_type == PT_LOAD && program_headers[i].p_flags & PF_X)
             break;
-        program_headers++;
     }
 
-	Elf64_Phdr *next_header = program_headers + 1;
+	Elf64_Phdr *selected_header = &program_headers[i];
+	Elf64_Phdr *next_header = &program_headers[i+1];
 
-	if(next_header->p_type != PT_LOAD || program_headers->p_type != PT_LOAD) {
-		printf("Error: PT_LOAD not found\n");
-		return;
-	}
-
-	if(program_headers->p_memsz != program_headers->p_filesz || (CODE_OFFSET + CODE_SIZE) > (next_header->p_offset + program_headers->p_offset)) {
+	if(selected_header->p_memsz != selected_header->p_filesz || (CODE_OFFSET + CODE_SIZE) > (next_header->p_offset + selected_header->p_offset)) {
 		printf("Error: not enough space\n");
 		return;
 	}
 
-	printf("Found two PT_LOAD\n");
-	printf("First PT_LOAD: Start: 0x%.8lx, End: 0x%.8lx\n", program_headers->p_offset, program_headers->p_offset + program_headers->p_memsz);
+	printf("Found two PT_LOAD (%i and %i)\n", i, i+1);
+	printf("First PT_LOAD: Start: 0x%.8lx, End: 0x%.8lx\n", selected_header->p_offset, selected_header->p_offset + selected_header->p_memsz);
 	printf("Second PT_LOAD: Start: 0x%.8lx, End: 0x%.8lx\n", next_header->p_offset, next_header->p_offset + next_header->p_memsz);
-	printf("Between them, there is %lu bytes (for a payload of %lu bytes)\n", next_header->p_offset - (program_headers->p_offset + program_headers->p_memsz), CODE_SIZE);
+	printf("Between them, there is %lu bytes (for a payload of %lu bytes)\n", next_header->p_offset - (selected_header->p_offset + selected_header->p_memsz), CODE_SIZE);
 
-	header->e_entry = program_headers->p_vaddr + program_headers->p_memsz;
+	header->e_entry = selected_header->p_vaddr + selected_header->p_memsz;
 	printf("Old entry point: 0x%.8x\n", last_entry);
 	printf("New entry point: 0x%.8lx\n", header->e_entry);
 
@@ -78,8 +71,8 @@ void create_codecave(Elf64_Ehdr *header, Elf64_Shdr *section_headers, Elf64_Phdr
 
 	insert_code(output_file_map + CODE_OFFSET);
 
-	program_headers->p_memsz += CODE_SIZE;
-    program_headers->p_filesz += CODE_SIZE;    
+	selected_header->p_memsz += CODE_SIZE;
+    selected_header->p_filesz += CODE_SIZE;
 
-	printf("New PT_LOAD: Start: 0x%.8lx, End: 0x%.8lx\n", program_headers->p_offset, program_headers->p_offset + program_headers->p_memsz);
+	printf("New PT_LOAD: Start: 0x%.8lx, End: 0x%.8lx\n", selected_header->p_offset, selected_header->p_offset + selected_header->p_memsz);
 }
