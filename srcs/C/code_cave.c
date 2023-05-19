31ed0ec75d5489028d9696c3d9d730f6aebb37a7
Damien <hubleur.damien333@protonmail.ch>
2023-05-19 15:08:11 +0200

    clean

--- srcs/C/code_cave.c
+++ srcs/C/code_cave.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:54:29 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/05/17 13:24:00 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/05/19 15:07:47 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ char payload[] = "\x31\xc0\x99\xb2\x0a\xff\xc0\x89"
                    "\x0f\x05\x2e\x2e\x57\x4f\x4f\x44"
                    "\x59\x2e\x2e\x0a";
 
-// jmp 0x00000000
+// jmp 0x00000000 (to be replaced)
 char jmp[] = "\xe9\x00\x00\x00\x00";  
 
 // push rax; push rdi; push rsi
@@ -42,9 +42,7 @@ char pusha[] = "\x50\x57\x56";
 // pop rsi; pop rdi; pop rax
 char popa[] = "\x5e\x5f\x58";
 
-#define CODE_SIZE (sizeof(payload)-1 + sizeof(jmp)-1 + sizeof(pusha)-1 + sizeof(popa)-1)
-
-#define CODE_OFFSET (selected_header->p_offset + selected_header->p_memsz)
+#define PAYLOAD_LENGTH (sizeof(payload)-1 + sizeof(jmp)-1 + sizeof(pusha)-1 + sizeof(popa)-1)
 
 void insert_code(unsigned char *ptr)
 {
@@ -56,7 +54,7 @@ void insert_code(unsigned char *ptr)
    
     memcpy(ptr, popa, sizeof(popa)-1);
     ptr += sizeof(popa)-1;
-   
+
     memcpy(ptr, jmp, sizeof(jmp)-1);
 }
 
@@ -76,7 +74,9 @@ void create_codecave(Elf64_Ehdr *header, Elf64_Shdr *section_headers, Elf64_Phdr
 	Elf64_Phdr *selected_header = &program_headers[i];
 	Elf64_Phdr *next_header = &program_headers[i+1];
 
-	if(selected_header->p_memsz != selected_header->p_filesz || (CODE_OFFSET + CODE_SIZE) > (next_header->p_offset + selected_header->p_offset)) {
+	Elf64_Off payload_offset = selected_header->p_offset + selected_header->p_memsz;
+
+	if(selected_header->p_memsz != selected_header->p_filesz || (payload_offset + PAYLOAD_LENGTH) > (next_header->p_offset + selected_header->p_offset)) {
 		printf("Error: not enough space\n");
 		return;
 	}
@@ -84,20 +84,20 @@ void create_codecave(Elf64_Ehdr *header, Elf64_Shdr *section_headers, Elf64_Phdr
 	printf("Found two PT_LOAD (%i and %i)\n", i, i+1);
 	printf("First PT_LOAD: Start: 0x%.8lx, End: 0x%.8lx\n", selected_header->p_offset, selected_header->p_offset + selected_header->p_memsz);
 	printf("Second PT_LOAD: Start: 0x%.8lx, End: 0x%.8lx\n", next_header->p_offset, next_header->p_offset + next_header->p_memsz);
-	printf("Between them, there is %lu bytes (for a payload of %lu bytes)\n", next_header->p_offset - (selected_header->p_offset + selected_header->p_memsz), CODE_SIZE);
+	printf("Between them, there is %lu bytes (for a payload of %lu bytes)\n", next_header->p_offset - (selected_header->p_offset + selected_header->p_memsz), PAYLOAD_LENGTH);
 
 	header->e_entry = selected_header->p_vaddr + selected_header->p_memsz;
 	printf("Old entry point: 0x%.8x\n", last_entry);
 	printf("New entry point: 0x%.8lx\n", header->e_entry);
 
-	int32_t jmp_adr = (int32_t)(last_entry - (header->e_entry + CODE_SIZE));
+	int32_t jmp_adr = (int32_t)(last_entry - (header->e_entry + PAYLOAD_LENGTH));
 	memcpy(jmp + 1, &jmp_adr, sizeof(int32_t));
 	printf("JMP address: 0x%.8x\n", jmp_adr);
 
-	insert_code(output_file_map + CODE_OFFSET);
+	insert_code(output_file_map + payload_offset);
 
-	selected_header->p_memsz += CODE_SIZE;
-    selected_header->p_filesz += CODE_SIZE;
+	selected_header->p_memsz += PAYLOAD_LENGTH;
+    selected_header->p_filesz += PAYLOAD_LENGTH;
 
 	printf("New PT_LOAD: Start: 0x%.8lx, End: 0x%.8lx\n", selected_header->p_offset, selected_header->p_offset + selected_header->p_memsz);
 }
