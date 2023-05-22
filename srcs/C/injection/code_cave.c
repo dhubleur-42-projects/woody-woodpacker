aac14635cf182e799d3b54420e0732f0a06fb0e2
Damien <hubleur.damien333@protonmail.ch>
2023-05-22 13:56:25 +0200

    Remove unused comment

--- srcs/C/injection/code_cave.c
+++ srcs/C/injection/code_cave.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:54:29 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/05/22 13:48:20 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/05/22 13:56:10 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 size_t use_code_cave(Elf64_Ehdr *header, Elf64_Phdr *code_cave_header, size_t payload_size) {
 	header->e_entry = code_cave_header->p_vaddr + code_cave_header->p_memsz;
-	printf("Moved entry point to 0x%.8lx\n", header->e_entry);
 	code_cave_header->p_memsz += payload_size;
     code_cave_header->p_filesz += payload_size;
 	return code_cave_header->p_offset + code_cave_header->p_memsz - payload_size;
