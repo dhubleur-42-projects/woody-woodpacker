ee28ae84b983575538746d1469669c9a3da954cd
Damien <hubleur.damien333@protonmail.ch>
2023-05-22 13:52:31 +0200

    Remove commentend main

--- srcs/C/main.c
+++ srcs/C/main.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:02:19 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/05/22 13:34:04 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/05/22 13:52:20 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 
 int	main(int ac, char **av) {
 	(void)ac;
-	// uint8_t	key[32];
+	uint8_t	key[32];
 
-	// if (ac < 2 || (ac >= 3 && (ft_strlen(av[2]) != 66 || !is_valid_hex(av[2]))))
-	// {
-	// 	dprintf(2, "Usage: %s <filename> [encryption_key (32 bytes long key: 0x0123456789ABCDEF..)]\n", av[0]);
-	// 	return (1);
-	// }
-	// get_key(key, ac, av);
+	if (ac < 2 || (ac >= 3 && (ft_strlen(av[2]) != 66 || !is_valid_hex(av[2]))))
+	{
+		dprintf(2, "Usage: %s <filename> [encryption_key (32 bytes long key: 0x0123456789ABCDEF..)]\n", av[0]);
+		return (1);
+	}
+	get_key(key, ac, av);
 	start_injection(av[1]);
 	return (0);
 }
