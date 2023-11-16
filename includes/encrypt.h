f9b12342d02bbdcf470ea14862fc6890a5f3aee2
dhubleur <dhubleur@student.42.fr>
2023-11-16 18:28:52 +0100

    add missing header protection

--- includes/encrypt.h
+++ includes/encrypt.h
@@ -6,11 +6,16 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:40:11 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/11/16 15:35:10 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/11/16 18:28:32 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef ENCRYPT_H
+# define ENCRYPT_H
 
 # include <elf.h>
 # include <string.h>
 
 void xor_cipher(char *data, int size, char *key);
+
+#endif
