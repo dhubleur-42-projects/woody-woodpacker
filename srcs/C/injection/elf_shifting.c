d3ee2eebeea5f651a621d4e257517b92a1097210
dhubleur <dhubleur@student.42.fr>
2023-11-17 15:31:24 +0100

    start fixing of elf shifting

--- srcs/C/injection/elf_shifting.c
+++ srcs/C/injection/elf_shifting.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 13:43:11 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/11/16 17:27:26 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/11/17 15:30:15 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ size_t get_extend_size_elf64(size_t payload_length) {
 	return (extend_size);
 }
 
-void create_new_section_header_elf64(size_t payload_length, Elf64_Ehdr *header, Elf64_Shdr *section_headers, Elf64_Shdr *new_section_header) {
+void create_new_section_header_elf64(size_t payload_length, Elf64_Ehdr *header, Elf64_Shdr *section_headers, Elf64_Phdr *programs_headers, Elf64_Shdr *new_section_header) {
 	Elf64_Shdr *last_section = &section_headers[header->e_shnum - 1];
 	new_section_header->sh_name = 1;
 	new_section_header->sh_type = SHT_PROGBITS;
-	new_section_header->sh_flags = SHF_ALLOC | SHF_EXECINSTR;
+	new_section_header->sh_flags = SHF_ALLOC | SHF_EXECINSTR | SHF_WRITE | SHF_GROUP | SHF_TLS;
 	new_section_header->sh_addr = last_section->sh_addr + last_section->sh_size;
 	new_section_header->sh_offset = last_section->sh_offset + last_section->sh_size;
 	new_section_header->sh_size = payload_length;
@@ -31,6 +31,15 @@ void create_new_section_header_elf64(size_t payload_length, Elf64_Ehdr *header, 
 	new_section_header->sh_info = 0;
 	new_section_header->sh_addralign = 0;
 	new_section_header->sh_entsize = 0;
+
+	Elf64_Phdr *last_program = &programs_headers[header->e_phnum - 1];
+	unsigned long current_end = last_program->p_offset + last_program->p_filesz;
+	if (new_section_header->sh_offset + new_section_header->sh_size > current_end)
+	{
+		last_program->p_filesz += new_section_header->sh_offset + new_section_header->sh_size - current_end;
+		last_program->p_memsz += new_section_header->sh_offset + new_section_header->sh_size - current_end;
+	}
+	last_program->p_flags |= PF_W | PF_X | PF_R;
 
 	header->e_shnum++;
 }
@@ -44,16 +53,16 @@ void make_space_for_new_section_elf64(size_t payload_length, Elf64_Ehdr *header,
 	memmove(output_map + end_new_section, output_map + begin_new_section, size_to_move);
 	if (options.verbose)
 		printf("Shifted %ld bytes\n", size_to_move);
-	header->e_shoff += end_new_section - new_section_header->sh_offset;
+	header->e_shoff += end_new_section - begin_new_section;
 }
 
 size_t extend_and_shift_elf64(size_t payload_length, t_file_elf64 file, void *output_map, off_t old_file_size, t_injection *injection, t_options options) {
 	off_t diff = file.programs[0].p_vaddr - file.programs[0].p_offset;
 	Elf64_Shdr *new_section_header = &(file.sections[file.header->e_shnum]);
-	create_new_section_header_elf64(payload_length, file.header, file.sections, new_section_header);
-	make_space_for_new_section_elf64(payload_length, file.header, output_map, new_section_header, old_file_size, options);
+	create_new_section_header_elf64(payload_length, file.header, file.sections, file.programs, new_section_header);
 	file.header->e_entry = diff + new_section_header->sh_offset;
 	injection->new_entrypoint = file.header->e_entry;
-	return (new_section_header->sh_offset);
-
+	size_t payload_offset = new_section_header->sh_offset;
+	make_space_for_new_section_elf64(payload_length, file.header, output_map, new_section_header, old_file_size, options);
+	return (payload_offset);
 }
--- srcs/C/injection/prepare_injection_elf64.c
+++ srcs/C/injection/prepare_injection_elf64.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:58:19 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/11/16 17:59:22 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/11/17 15:03:58 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,8 +77,12 @@ bool	prepare_injection_elf64(t_file file, t_injection *injection, t_options opti
 	output_file.programs = (Elf64_Phdr *)(injection->file_map + output_file.header->e_phoff);
 	code_cave = find_code_cave_elf64(output_file, get_payload_length());
 	
-	if (code_cave == NULL)
+	if (code_cave == NULL) {
 		injection->payload_offset = extend_and_shift_elf64(get_payload_length(), output_file, injection->file_map, file.size, injection, options);
+		output_file.header = (Elf64_Ehdr *)injection->file_map;
+		output_file.sections = (Elf64_Shdr *)(injection->file_map + output_file.header->e_shoff);
+		output_file.programs = (Elf64_Phdr *)(injection->file_map + output_file.header->e_phoff);
+	}
 	else
 	{
 		injection->payload_offset = use_code_cave_elf64(output_file.header, code_cave, get_payload_length(), injection);
