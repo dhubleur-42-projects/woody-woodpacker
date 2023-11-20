92cc5293724179a37f5478626ac59d7365a02629
dhubleur <dhubleur@student.42.fr>
2023-11-20 18:06:46 +0100

    clean

--- srcs/C/injection/elf_shifting.c
+++ srcs/C/injection/elf_shifting.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 13:43:11 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/11/20 16:59:18 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/11/20 18:05:37 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ size_t get_extend_size_elf64(size_t payload_length, t_file_elf64 file_elf64) {
 	return (0);
 }
 
-Elf64_Phdr *extend_and_shift_elf64(size_t payload_length, char *map, size_t map_length, t_options options) {
+void	extend_and_shift_elf64(size_t payload_length, char *map, size_t map_length, t_options options) {
 	t_file_elf64 file_elf64;
 
 	file_elf64.header = (Elf64_Ehdr *)map;
@@ -43,16 +43,18 @@ Elf64_Phdr *extend_and_shift_elf64(size_t payload_length, char *map, size_t map_
 			continue;
 		if (file_elf64.programs[i + 1].p_type != PT_LOAD)
 			continue;
-		size_t available_space = file_elf64.programs[i + 1].p_offset - (file_elf64.programs[i].p_offset + file_elf64.programs[i].p_memsz);
-		off_t last_address_used = file_elf64.programs[i].p_offset + file_elf64.programs[i].p_memsz + payload_length;
+
 		if (options.verbose)
 			printf("Extend segment at 0x%.8lx and use it as a code cave\n", file_elf64.programs[i].p_offset);
+
+		off_t last_address_used = file_elf64.programs[i].p_offset + file_elf64.programs[i].p_memsz + payload_length;
 		size_t bytes_used_to_align = file_elf64.programs[i + 1].p_align - last_address_used % file_elf64.programs[i + 1].p_align;
 		off_t new_offset = last_address_used + bytes_used_to_align;
 		unsigned long old_offset = file_elf64.programs[i + 1].p_offset;
 		off_t shifting = new_offset - old_offset;
 		if (options.verbose)
 			printf("Shifting %ld bytes with an offset of %ld\n", map_length - old_offset, shifting);
+
 		for (int j = 0; j < file_elf64.header->e_shnum; j++) {
 			if (file_elf64.sections[j].sh_offset >= old_offset)
 				file_elf64.sections[j].sh_offset += shifting;
@@ -64,11 +66,12 @@ Elf64_Phdr *extend_and_shift_elf64(size_t payload_length, char *map, size_t map_
 		file_elf64.header->e_shoff += shifting;
 		memmove(map + new_offset, map + old_offset, map_length - old_offset);
 		bzero(map + old_offset, shifting);
-		file_elf64.programs = (Elf64_Phdr *)(map + file_elf64.header->e_phoff);
-		available_space = file_elf64.programs[i + 1].p_offset - (file_elf64.programs[i].p_offset + file_elf64.programs[i].p_memsz);
+		
 		if (options.verbose)
+		{
+			file_elf64.programs = (Elf64_Phdr *)(map + file_elf64.header->e_phoff);
+			size_t available_space = file_elf64.programs[i + 1].p_offset - (file_elf64.programs[i].p_offset + file_elf64.programs[i].p_memsz);
 			printf("New available space: %lu (between 0x%.8lx and 0x%.8lx)\n", available_space, file_elf64.programs[i].p_offset + file_elf64.programs[i].p_memsz, file_elf64.programs[i + 1].p_offset);
-		return (&file_elf64.programs[i]);
+		}
 	}
-	return (NULL);
 }
--- srcs/C/injection/prepare_injection_elf64.c
+++ srcs/C/injection/prepare_injection_elf64.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:58:19 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/11/20 16:34:53 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/11/20 18:06:43 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,10 @@ Elf64_Shdr *get_section(char *name, Elf64_Ehdr *header, Elf64_Shdr *section_head
 bool	prepare_injection_elf64(t_file file, t_injection *injection, t_options options)
 {
 	t_file_elf64 file_efl64 = *((t_file_elf64 *)file.specific_file);
+	injection->old_entrypoint = file_efl64.header->e_entry;
+
 	Elf64_Phdr *code_cave = find_code_cave_elf64(file_efl64, get_payload_length());
 	size_t expand_size = 0;
-	injection->old_entrypoint = file_efl64.header->e_entry;
 	if (code_cave != NULL)
 	{
 		if (options.verbose)
@@ -55,6 +56,7 @@ bool	prepare_injection_elf64(t_file file, t_injection *injection, t_options opti
 			return false;
 		}
 	}
+
 	injection->fd = open("woody", O_CREAT | O_RDWR | O_TRUNC, 0777);
 	if (injection->fd == -1)
 	{
@@ -77,20 +79,18 @@ bool	prepare_injection_elf64(t_file file, t_injection *injection, t_options opti
 		printf("Mapped a file of %lu bytes (extanded by %lu bytes)\n", injection->file_size, expand_size);
 	memcpy(injection->file_map, file.map, file.size);
 
-	Elf64_Phdr *new_code_cave = NULL;
 	if (code_cave == NULL)
-		new_code_cave = extend_and_shift_elf64(get_payload_length(), injection->file_map, file.size, options);
+		extend_and_shift_elf64(get_payload_length(), injection->file_map, file.size, options);
 
 	t_file_elf64 output_file;
 	output_file.header = (Elf64_Ehdr *)injection->file_map;
 	output_file.sections = (Elf64_Shdr *)(injection->file_map + output_file.header->e_shoff);
 	output_file.programs = (Elf64_Phdr *)(injection->file_map + output_file.header->e_phoff);
-	if (new_code_cave == NULL)
-		new_code_cave = find_code_cave_elf64(output_file, get_payload_length());
-	
-	injection->payload_offset = use_code_cave_elf64(output_file.header, new_code_cave, get_payload_length(), injection);
+
+	code_cave = find_code_cave_elf64(output_file, get_payload_length());
+	injection->payload_offset = use_code_cave_elf64(output_file.header, code_cave, get_payload_length(), injection);
 	if (options.verbose)
-		printf("Code cave header modified, new end: 0x%.8lx\n", new_code_cave->p_offset + new_code_cave->p_memsz);
+		printf("Code cave header modified, new end: 0x%.8lx\n", code_cave->p_offset + code_cave->p_memsz);
 
 	Elf64_Shdr *text_section = get_section(".text", output_file.header, output_file.sections);
 	if (text_section == NULL)
