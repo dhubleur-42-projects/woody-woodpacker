0cd11a2e9f1a267a298c065703df9f317f401ad6
dhubleur <dhubleur@student.42.fr>
2023-11-23 16:17:18 +0100

    clean

--- srcs/C/injection/elf_shifting.c
+++ srcs/C/injection/elf_shifting.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 13:43:11 by dhubleur          #+#    #+#             */
-/*   Updated: 2023/11/22 17:40:05 by dhubleur         ###   ########.fr       */
+/*   Updated: 2023/11/23 15:44:14 by dhubleur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,14 +64,19 @@ void	extend_and_shift_elf64(size_t payload_length, char *map, size_t map_length,
 
 	for (int j = 0; j < file_elf64.header->e_shnum; j++) {
 		if (file_elf64.sections[j].sh_offset >= old_offset)
+		{
 			file_elf64.sections[j].sh_offset += shifting;
+		}
 	}
 	for (int j = 0; j < file_elf64.header->e_phnum; j++) {
 		if (file_elf64.programs[j].p_offset >= old_offset)
+		{
 			file_elf64.programs[j].p_offset += shifting;
+		}
 	}
 	file_elf64.programs[code_cave_segment_idx + 1].p_vaddr += shifting;
 
+	file_elf64.programs[code_cave_segment_idx].p_flags |= PF_X;
 	file_elf64.programs[code_cave_segment_idx + 1].p_flags |= PF_X;
 	
 	if (file_elf64.header->e_shoff >= old_offset)
