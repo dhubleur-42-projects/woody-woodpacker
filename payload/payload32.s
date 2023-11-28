8df53cee9c539de8668f48b2455d30b557867de4
dhubleur <dhubleur@student.42.fr>
2023-11-28 15:09:09 +0100

    update payload for 32bits registers

--- payload/payload32.s
+++ payload/payload32.s
@@ -5,61 +5,57 @@ global _start
 _start:
 		; save registers
 		push eax
-		push edi
-		push edx
 		push ebx
 		push ecx
+		push edx
 
 		; print message
-		xor eax, eax
 		cdq
 		mov dl, 10
-		inc eax
 		mov ebx, 1
 		lea ecx, [rel msg]
 		mov eax, 4
 		int 0x80
 
 		; uncipher
-		lea edi, [ecx + (_start - msg)]         ; payload address
-		sub edi, [ecx + (text_offset - msg)]    ; edi = .text address
-		mov ecx, [data_len]                     ; ecx = .text size
-		lea edx, [key]                          ; edx = key address
+		lea eax, [ecx + (_start - msg)]         ; payload address
+		sub eax, [ecx + (text_offset - msg)]    ; eax = .text address
+		mov ebx, [data_len]                     ; ebx = .text size
+		lea ecx, [key]                          ; ecx = key address
 
-		push edx
+		push ecx
 		jmp xor_loop
 
 		xor_loop:
-			cmp ecx, 0           ; Check if size is 0
+			cmp ebx, 0           ; Check if size is 0
 			je xor_end
 
-			mov al, [edi]        ; Load current byte of data
-			mov bl, [edx]        ; Load current byte of key
+			mov al, [eax]        ; Load current byte of data
+			mov bl, [ecx]        ; Load current byte of key
 			xor al, bl           ; XOR operation
-			mov [edi], al        ; Store the result back in data
+			mov [eax], al        ; Store the result back in data
 
-			inc edi              ; Increment data pointer
-			inc edx              ; Increment key pointer
+			inc eax              ; Increment data pointer
+			inc ecx              ; Increment key pointer
 
-			dec ecx              ; Decrement size
+			dec ebx              ; Decrement size
 
-			cmp byte [edx], 0    ; Check if the current byte of key is '\0'
+			cmp byte [ecx], 0    ; Check if the current byte of key is '\0'
 			je reset_key
 
 			jmp xor_loop
 
 		reset_key:
-			pop edx
+			mov ecx, [esp] 
 
 			jmp xor_loop
 
 		xor_end:
-			pop ebx
+			pop ecx
 
+			pop edx
 			pop ecx
 			pop ebx
-			pop edx
-			pop edi
 			pop eax
 
 			jmp 0x00000000
